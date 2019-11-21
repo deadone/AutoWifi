a8d715f906ad0814b77f099b3d544eb0bfd5cf0f
deadone <45006877+deadone@users.noreply.github.com>
2019-11-21 07:00:12 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@
 RET=$PWD
 echo "**************** -=[   Installing AutoWifi...    ]=- ****************"
 echo "**************** -=[ Configurting Directories... ]=- ****************"
+# move folders to opt folder for ease of collection and removal, and set privs
 sudo mv autowifi/ /opt/
 sudo mv hcxdumptool/ /opt/
 sudo mv hcxtools/ /opt/
@@ -15,16 +16,20 @@ sleep 2s
 echo "[ *** DONE **** ]"
 echo ""
 echo "**************** -=[ Creating AutoWifi Shortcut... ]=- ****************"
+# move deafi to /bin/ and rename it autowifi, and give it priv. this program needs
+# to be run as root to interact with interfaces
 sudo cp deadfi /bin/autowifi
 sudo chmod 755 /bin/autowifi
 sleep 2s
 echo "[ *** DONE **** ]"
 echo ""
 echo "**************** -=[ Installing Dependencies... ]=- ****************"
+# using trustesd depositories, download the required packages
 sudo apt install -y hashcat mdk3 aircrack-ng libcurl4-openssl-dev libssl-dev zlib1g-dev
 echo "[ *** DONE **** ]"
 echo ""
 echo "**************** -=[ Installing HCXDump Tools... ]=- ****************"
+# installs hcxdumptool, there is a link to the repository on the readme.
 sleep 2s
 cd /opt/hcxdumptool/
 sudo make
@@ -32,6 +37,7 @@ sudo make install
 echo "[ *** DONE **** ]"
 echo ""
 echo "**************** -=[ Installing HCX Tools... ]=- ****************"
+# installs hcxtools, there is a link to the repository on the readme. 
 sleep 2s
 cd /opt/hcxtools/
 sudo make
@@ -39,6 +45,8 @@ sudo make install
 echo "[ *** DONE **** ]"
 echo ""
 echo "**************** -=[ Cleaning Up Installation Files... ]=- ****************"
+# delete github place.holders for directory creation, and return original installation folder, 
+# everything exists in /opt/autowifi/ now, and /opt/hcxtools /opt/hcxdumptool.
 sudo rm /opt/autowifi/airo/place.holder
 sudo rm /opt/autowifi/handshakes/place.holder
 sudo rm /opt/autowifi/loot/place.holder
@@ -50,13 +58,19 @@ sleep 2s
 echo "[ *** DONE **** ]"
 echo ""
 echo "*******************************************************************"
-echo "|                       dead1's AutoWifi                          |"
+echo "|                        dead1's AutoWifi                         |"
+echo "|                                                                 |"
 echo "|                 -= Enjoy. Use Responsibly =-                    |"
 echo "|      I am not responsible for anything you do with this,        |"
 echo "| I am not responsible for anything that happens from using this. |"
 echo "|               ****** USE AT YOUR OWN RISK ******                |"
-echo "|                       Use:    autowifi                          |"
-echo "|                                            ... dead1.           |"
+echo "|              PLEASE READ THE README.md ON GITHUB                |"
+echo "|           autowifi and Capture files can be found in:           |"
+echo "|                          /opt/autowifi                          |"
+echo "|                                                                 |"
+echo "|                To use autowifi:    autowifi                     |"
+echo "|                        (requires root)                          |"
+echo "|                                                    ... dead1.   |"
 echo "*******************************************************************"
 echo ""
 
